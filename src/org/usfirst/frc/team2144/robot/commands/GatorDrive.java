2c252760e361fbc741daf6808b2093b8bc78a7f4
mommothazaz123 <mommothazaz123@gmail.com>
2016-01-07 14:20:33 -0800

    few random derps

--- src/org/usfirst/frc/team2144/robot/commands/GatorDrive.java
+++ src/org/usfirst/frc/team2144/robot/commands/GatorDrive.java
@@ -1,43 +1,36 @@
 
 package org.usfirst.frc.team2144.robot.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
-
-import org.usfirst.frc.team2144.robot.OI;
-import org.usfirst.frc.team2144.robot.Robot;
-import org.usfirst.frc.team2144.robot.subsystems.Drivetrain;
-import org.usfirst.frc.team2144.robot.commands.CommandBase;
-
 /**
  *
  */
 public class GatorDrive extends CommandBase {
 
-    public GatorDrive() {
-        // Use requires() here to declare subsystem dependencies
-        requires(drivetrain);
-    }
+	public GatorDrive() {
+		// Use requires() here to declare subsystem dependencies
+		requires(drivetrain);
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	drivetrain.gatorDrive(oi.getPrecise(), oi.getb4(), oi.getb5(), oi.getStickX(), oi.getStickY()); 
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		drivetrain.gatorDrive(oi.getPrecise(), oi.getb4(), oi.getb5(), oi.getStickX(), oi.getStickY());
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 }
